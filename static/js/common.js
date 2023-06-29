2595f4b032e274bf8aced4f9600a7496d59b3203
ug <dbwldms25@gmail.com>
2023-06-29 15:10:07 +0900

    feat: 로그인시 header 버튼 변경되는 기능 추가

--- static/js/common.js
+++ static/js/common.js
@@ -1,13 +1,27 @@
 // header 메뉴 분기점
 if (document.cookie.includes("Authorization")) {
-  console.log("로그인 되어있습니다.")
-  document.querySelector(".btn-login").classList.add("hidden");
-  document.querySelector(".btn-my-page").classList.remove("hidden");
-
+  if (location.pathname === "/") {
+    document.querySelector('.right-btn-box').innerHTML = `
+      <a href="html/create-content.html">작성</a>
+      <a href="html/my-page.html" class="btn-my-page">마이페이지</a>
+    `;
+  } else {
+    document.querySelector('.right-btn-box').innerHTML = `
+      <a href="create-content.html">작성</a>
+      <a href="my-page.html" class="btn-my-page">마이페이지</a>
+    `;
+  }
 } else {
-  console.log("게스트 입니다.")
-  document.querySelector(".btn-login").classList.remove("hidden");
-  document.querySelector(".btn-my-page").classList.add("hidden");
+  console.log("게스트 입니다.");
+  if (location.pathname === "/") {
+    document.querySelector('.right-btn-box').innerHTML = `
+      <a href="html/create-content.html">작성</a>
+      <a href="html/sign-in.html" class="btn-login">로그인</a>
+    `;
+  } else {
+    document.querySelector('.right-btn-box').innerHTML = `
+      <a href="create-content.html">작성</a>
+      <a href="sign-in.html" class="btn-login">로그인</a>
+    `;
+  }
 }
-
-
