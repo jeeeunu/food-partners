2a8ca9ebb984851244af09beb7d1d85b753b32c2
Lee <jrmun123@gmail.com>
2023-06-29 15:23:39 +0900

    fix: 사진이 없는 상태에서 유효성검사에 실패할 경우 오류

--- static/js/my-page.js
+++ static/js/my-page.js
@@ -1,22 +1,20 @@
 function fetchData(url, options) {
   return fetch(url, options)
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
         throw new Error('마이페이지 회원정보조회 실패');
       }
       return response.json();
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
     });
 }
 
 const fetchAndDisplayUser = () => {
-  fetchData('/api/userInfo', {
-  })
-    .then(data => {
-      const userInformation = document.querySelector('main');
-      userInformation.innerHTML = `
+  fetchData('/api/userInfo', {}).then((data) => {
+    const userInformation = document.querySelector('main');
+    userInformation.innerHTML = `
             <p>Email: ${data.data.email}</p>
             <p>Profile Picture: ${data.data.profilepicture}</p>
             <p>Nickname: ${data.data.nickname}</p>
@@ -26,7 +24,7 @@ const fetchAndDisplayUser = () => {
             <p>Introduce: ${data.data.introduce}</p>
             <p>Last Updated At: ${data.data.updatedAt}</p>
           `;
-    });
-}
+  });
+};
 
 window.onload = fetchAndDisplayUser;
