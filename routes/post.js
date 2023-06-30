ec1e616198c89394adf66f9584269eef1dbdaf2a
ug <dbwldms25@gmail.com>
2023-06-30 12:17:48 +0900

    fix: 파일 통일

--- routes/post.js
+++ routes/post.js
@@ -30,63 +30,98 @@ router.post('/posts', authMiddleware, upload, async (req, res) => {
   res.status(201).json({ data: post });
 });
 
-// GET
 router.get('/posts', async (req, res) => {
   const posts = await Posts.findAll({
-    attributes: ['title', 'createdAt', 'updatedAt'],
+    attributes: ['title', 'thumbnail', 'content', 'createdAt'],
+    include: [
+      {
+        model: Users,
+        attributes: ['nickname'],
+      },
+    ],
     order: [['createdAt', 'DESC']],
   });
-
   return res.status(200).json({ data: posts });
 });
 
-// GET : myPost
-router.get('/myPost', authMiddleware, async (req, res) => {
-  const userid = res.locals.user.userid;
-
-  const post = await Posts.findOne({
-    attributes: ['email', 'password', 'profilepicture', 'nickname', 'birth', 'gender', 'address', 'introduce', 'updatedAt'],
-    where: { userid },
-  });
-});
-
+// GET
 router.get('/posts/:postId', async (req, res) => {
   const { postId } = req.params;
   const post = await Posts.findOne({
-    attributes: ['postId', 'title', 'content', 'createdAt', 'updatedAt'],
-    where: { postId },
+    attributes: ['title', 'thumbnail', 'content', 'createdAt', 'updatedAt'],
+    where: { postId: postId },
   });
-
   return res.status(200).json({ data: post });
 });
 
-router.delete('/posts/:postId', async (req, res) => {
+// DELETE
+router.delete('/posts/:postId', authMiddleware, async (req, res) => {
   const { postId } = req.params;
+  const UserId = res.locals.user.userid;
+  const post = await Posts.findOne({ where: { postId: postId } });
 
-  const post = await Posts.findOne({ where: { postId } });
+  if (!post) {
+    return res.status(404).json({ message: '게시글이 존재하지 않습니다.' });
+  } else if (post.UserId !== UserId) {
+    return res.status(401).json({ message: '권한이 없습니다.' });
+  }
 
   await Posts.destroy({
-    where: { postId },
+    where: { [Op.and]: [{ postId: postId }, { UserId: UserId }] },
   });
   return res.status(200).json({ data: '게시글이 삭제되었습니다.' });
 });
 
-router.put('/posts/:postId', async (req, res) => {
+// PUT
+router.put('/posts/:postId', authMiddleware, upload, async (req, res) => {
   const { postId } = req.params;
   const { title, content } = req.body;
-  const updatedAt = Date();
-  const post = await Posts.findOne({ where: { postId } });
+  const UserId = res.locals.user.userid;
+  const post = await Posts.findOne({ where: { postId: postId } });
+  if (!post) {
+    return res.status(404).json({ message: '게시글이 존재하지 않습니다.' });
+  } else if (post.UserId !== UserId) {
+    return res.status(401).json({ message: '권한이 없습니다.' });
+  }
 
-  await Posts.update(
-    { title, content, updatedAt }, // title과 content 컬럼을 수정합니다.
-    {
-      where: {
-        postId,
-      },
+  let thumbnail = req.file;
+  if (thumbnail) {
+    thumbnail = path.join('img-server', req.file.filename);
+  }
+
+  if (title.length === 0) {
+    res.status(400).json({
+      errorMessage: '제목을 작성해주세요.',
+    });
+    if (thumbnail) {
+      fs.unlinkSync('./img-server/' + req.file.filename);
     }
-  );
+    return;
+  }
 
-  return res.status(200).json({ data: '게시글이 수정되었습니다.' });
+  if (content.lenght === 0) {
+    res.status(400).json({
+      errorMessage: '내용을 작성해주세요.',
+    });
+    if (thumbnail) {
+      fs.unlinkSync('./img-server/' + req.file.filename);
+    }
+    return;
+  }
+
+  const existuser = await Posts.findOne({ where: { postId: postId } });
+
+  if (existuser) {
+    if (thumbnail) {
+      if (fs.existsSync(existuser.thumbnail)) {
+        fs.unlinkSync(existuser.thumbnail);
+      }
+    }
+    await Posts.update({ title, thumbnail, content }, { where: { postId: postId } });
+    res.status(200).json({ data: '게시글이 수정되었습니다.' });
+  } else {
+    res.status(400).json({ success: 'false', errorMessage: '오류가 발생했습니다.' });
+  }
 });
 
 module.exports = router;
