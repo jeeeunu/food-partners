d9ea71c09775b5da57289d9abdfc789e8b60673a
Lee <jrmun123@gmail.com>
2023-06-30 11:21:29 +0900

    feat: 테이블 관계 추가

--- models/users.js
+++ models/users.js
@@ -8,7 +8,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.hasMany(models.Posts, {
+        // 2. Posts 모델에게 1:N 관계 설정을 합니다.
+        sourceKey: 'userId', // 3. Users 모델의 userId 컬럼을
+        foreignKey: 'UserId', // 4. Posts 모델의 UserId 컬럼과 연결합니다.
+      });
     }
   }
   Users.init(
