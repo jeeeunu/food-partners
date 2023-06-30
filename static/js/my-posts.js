c5c2abb00463d44e5fca9bd704ce8e1cfe0b93e4
ug <dbwldms25@gmail.com>
2023-06-30 15:36:37 +0900

    feat: style.css 수정/내가 쓴글 프론트 추가

--- static/js/my-posts.js
+++ static/js/my-posts.js
@@ -2,34 +2,37 @@ import { fetchData } from './utils.js';
 
 //  내 작성글 조회
 const myPosts = () => {
-  fetchData('/api/myPost', { method: 'GET' }).then((data) => {
-    const postList = document.querySelector('#default_post_img');
+  fetchData('/api/myPost', { method: 'GET' }).then((response) => {
+    const { data } = response;
+    const { thumbnail } = data;
+    console.log(thumbnail);
+    const postList = document.querySelector('.card-container');
     postList.innerHTML = '';
-    const { title, createdAt, updatedAt } = data.result;
 
     // 이미지
     let profileImage = '';
-    if (profilepicture) {
+    if (thumbnail) {
       profileImage = `<img src="/${profilepicture.replace(/\\/g, '/')}" alt="프로필 이미지">`;
     } else {
-      profileImage = `<img src="../images/default_img.png" alt="기본 프로필 이미지">`;
+      profileImage = `<img src="../images/default_post_img.png" alt="기본 프로필 이미지" style="display: block; width: 40%; object-fit: contain; margin: 0 auto">`;
     }
 
-    const htmlArray = posts.map((post) => {
-      const { img: postImg, title: postTitle, content: postContent } = post; // 프로퍼티 확인 필요
-      return `<ol class="card">
-                <ul>
-                  ${postImg}
-                </ul>
-                <ul>
-                  ${postTitle}
-                </ul>
-                <ul>
-                  ${postContent}
-                </ul>
-              </ol>`;
+    data.forEach((content) => {
+      const contentHtml = `
+        <div class="card-item">
+          <a href="">
+            <div class="img-wrap">
+              ${profileImage}
+            </div>
+            <div class="text-wrap">
+              <strong>${content.title}</strong>
+              <p>${content.content}</p>
+            </div>
+          </a>
+        </div>
+      `;
+      postList.innerHTML += contentHtml;
     });
-    cardList.innerHTML = htmlArray.join(''); // 데이터 담은 htmlArray를 문자열로 합쳐서 card-list에 넣음
   });
 };
 
