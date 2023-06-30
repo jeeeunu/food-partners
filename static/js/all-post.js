33224db56008a58eac7661ac3a6359d35732d8c1
ug <dbwldms25@gmail.com>
2023-06-30 17:09:02 +0900

    feat: index.html 뉴스피드 api 연결

---
+++ static/js/all-post.js
@@ -0,0 +1,38 @@
+import { fetchData } from './utils.js';
+
+//  내 작성글 조회
+const myPosts = () => {
+  fetchData('/api/posts', { method: 'GET' }).then((response) => {
+    const { data } = response;
+    const thumbnail = data;
+    console.log(thumbnail);
+    const postList = document.querySelector('.card-list');
+    postList.innerHTML = '';
+
+    data.forEach((content) => {
+      // 이미지
+      let profileImage = '';
+      if (content.thumbnail) {
+        profileImage = `<img src="/${content.thumbnail.replace(/\\/g, '/')}" alt="프로필 이미지">`;
+      } else {
+        profileImage = `<img src="../images/default_post_img.png" alt="기본 프로필 이미지" style="display: block; width: 40%; object-fit: contain; margin: 0 auto">`;
+      }
+      const contentHtml = `
+        <div class="card-item">
+          <a href="">
+            <div class="img-wrap">
+              ${profileImage}
+            </div>
+            <div class="text-wrap">
+              <strong>${content.title}</strong>
+              <p>${content.content}</p>
+            </div>
+          </a>
+        </div>
+      `;
+      postList.innerHTML += contentHtml;
+    });
+  });
+};
+
+myPosts();
