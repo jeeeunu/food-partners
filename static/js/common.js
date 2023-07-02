69be5601dacdff10c7778c959a87f82bef4400e3
ug <dbwldms25@gmail.com>
2023-07-02 13:20:14 +0900

    fix: header 수정 (기존 html 마크업때문에 js가 부자연스럽게 바뀌는 점)

--- static/js/common.js
+++ static/js/common.js
@@ -9,7 +9,6 @@ if (document.cookie.includes('Authorization')) {
     document.querySelector('.right-btn-box').innerHTML = `
       <a href="/html/create-post-page.html">글 작성</a>
       <a href="/html/my-page.html" class="btn-my-page">마이페이지</a>
-      <a href="/html/admin-panel.html">관리자 패널</a>
     `;
   } else {
     document.querySelector('.right-btn-box').innerHTML = `
@@ -26,7 +25,6 @@ if (document.cookie.includes('Authorization')) {
   } else if (location.pathname.startsWith('/api')) {
     document.querySelector('.right-btn-box').innerHTML = `
       <a href="/html/sign-in.html" class="btn-login">로그인</a>
-      <a href="/html/admin-login.html" class="btn-admin-login">관리자 로그인</a>
     `;
   } else {
     document.querySelector('.right-btn-box').innerHTML = `
